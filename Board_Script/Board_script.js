f961c59bcd8c19f9ddb8a5010795a537ebedbdac
Eli-Boaz Clapper <74557493+Hypergeometricdistribution@users.noreply.github.com>
2021-10-22 17:59:57 +0200

    chess_board complete
    nice

--- Board_Script/Board_script.js
+++ Board_Script/Board_script.js
@@ -1,2 +1,16 @@
 let chess_board = document.getElementById('Chess_board') //obtain chess_board element
-console.log(chess_board) // seems okay
+//console.log(chess_board) // seems okay
+
+let sq_type = ['white', 'black']
+
+for (let row = 0; row < 8; row++) {
+    for (let col = 0; col < 8; col++) {
+        row_mod = row%2
+        col_mod = col%2
+        mod = (row_mod + col_mod)%2
+        let sq = document.createElement('div')
+        sq.className = sq_type[mod]
+        chess_board.appendChild(sq)
+    }
+}
+
