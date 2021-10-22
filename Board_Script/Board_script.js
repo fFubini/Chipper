2802e2cb8380143e1b39c07cc81ad7f6de110bca
Eli-Boaz Clapper <74557493+Hypergeometricdistribution@users.noreply.github.com>
2021-10-22 19:40:58 +0200

    Ids per div
    added ids per div

--- Board_Script/Board_script.js
+++ Board_Script/Board_script.js
@@ -1,7 +1,8 @@
-let chess_board = document.getElementById('Chess_board') //obtain chess_board element
+let chess_board = document.getElementById('Chess_board') ;//obtain chess_board element
 //console.log(chess_board) // seems okay
 
-let sq_type = ['white', 'black']
+let sq_type = ['white', 'black'];
+let sq_ids = 0; //initiate variable starting at 0 and adding up after every loop
 
 for (let row = 0; row < 8; row++) {
     for (let col = 0; col < 8; col++) {
@@ -10,7 +11,9 @@ for (let row = 0; row < 8; row++) {
         mod = (row_mod + col_mod)%2
         let sq = document.createElement('div')
         sq.className = sq_type[mod]
+        sq.id = sq_ids
         chess_board.appendChild(sq)
+        sq_ids += 1
     }
-}
+};
 
